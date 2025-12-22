00e818bbb784653701759f035b188c20edac7ad9
Lkira80 <ana_es22@yahoo.es>
2025-12-22 12:33:10 +0100

    Set React router and basic pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Products from "./pages/Products";
+import Cart from "./pages/Cart";
+import Orders from "./pages/Orders";
+
 function App() {
   return (
-    <div>
-      <h1>E-commerce Frontend</h1>
-      <p>React Working</p>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav>
+      <Link to="/">Home</Link> |{" "}
+      <Link to="/products">Products</Link> |{" "}
+      <Link to="/cart">Cart</Link> |{" "}
+      <Link to="/orders">Orders</Link> |{" "}
+      <Link to="/login">Login</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
